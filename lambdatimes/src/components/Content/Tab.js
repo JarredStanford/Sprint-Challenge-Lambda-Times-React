2d9bad86cd22480cf5586ab1b9baadbff7d165a9
unknown <stanj89@gmail.com>
2019-06-07 12:28:06 -0400

    tabs selector working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  const activeChecker = () => {
+    if (props.tab === props.selectedTab) {
+      return "tab active-tab";
+    } else return "tab";
+  };
+
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+      className={activeChecker()}
+      onClick={() => props.selectTabHandler(props.tab)}
+      /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
     >
       {props.tab.toUpperCase()}
     </div>
